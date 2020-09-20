b52534a3d8fbb1259ac4560459dda8e27a8bc832
sumit24745 <sumit.24745@lpu.co.in>
2020-09-20 19:58:44 +0530

    Updates

---
+++ precache-manifest.5566c67c30a8565bc2eda285178ea126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5664d4687988caa5ddd745800e03ef54",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6e89737efbb082dcea4",
+    "url": "/monsters-rolodex/static/css/main.9b2d15b7.chunk.css"
+  },
+  {
+    "revision": "001f0fb8c375fd8a6d43",
+    "url": "/monsters-rolodex/static/js/2.86c41a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.86c41a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e89737efbb082dcea4",
+    "url": "/monsters-rolodex/static/js/main.4f0f0fa3.chunk.js"
+  },
+  {
+    "revision": "1acfa1034230f7c657e8",
+    "url": "/monsters-rolodex/static/js/runtime-main.a26d2dbb.js"
+  }
+]);
